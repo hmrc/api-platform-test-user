3b22614ad76ab7a24d70a91a4b6cdefd14126fb4
syed-ali-hmrc <al1.sy3d@outlook.com>
2022-12-28 11:56:16 +0000

    API-5556: Upgraded play bootstrap to version 7.12.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,21 +4,23 @@ import play.core.PlayVersion
 object AppDependencies {
   def apply(): Seq[ModuleID] = compile ++ test
   
-  val AkkaVersion = "2.6.19"
-  
+  val AkkaVersion = "2.6.20"
+  lazy val bootstrapVersion = "7.12.0"
+  lazy val mongoVersion = "0.74.0"
+
   lazy val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.25.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion,
     "uk.gov.hmrc"             %% "play-json-union-formatter"  % "1.15.0-play-28",
     "uk.gov.hmrc"             %% "domain"                     % "6.2.0-play-28",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.73.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % mongoVersion,
     "org.mindrot"             % "jbcrypt"                     % "0.4",
     "com.typesafe.play"       %% "play-json"                  % "2.9.2",
     "org.scalacheck"          %% "scalacheck"                 % "1.13.5"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.25.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.73.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % mongoVersion,
     "com.typesafe.play"       %% "play-test"                  % PlayVersion.current,
     "com.typesafe.akka"       %% "akka-stream-testkit"        % AkkaVersion,
     "org.mockito"             %% "mockito-scala-scalatest"    % "1.17.12",
