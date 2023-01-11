a56b638e8400907cb87042a39c4ff5a706aac13d
anjumabbas <khaakpaayali@gmail.com>
2023-01-11 15:21:09 +0000

    API-5498 - Copyright fix

--- app/uk/gov/hmrc/testuser/MicroserviceModule.scala
+++ app/uk/gov/hmrc/testuser/MicroserviceModule.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.testuser
 
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
