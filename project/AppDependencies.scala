a574a871d87192891ffc2333fa5d22c735ccdd08
John Green <john.green@digital.hmrc.gov.uk>
2024-02-12 15:40:01 +0000

    APIS-6799: PR Bot recommendations and other evergreening

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,14 +14,14 @@ object AppDependencies {
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % mongoVersion,
     "org.mindrot"        % "jbcrypt"                    % "0.4",
     "uk.gov.hmrc"       %% "api-platform-common-domain" % "0.10.0",
-    "org.scalacheck"    %% "scalacheck"                 % "1.14.1"
+    "org.scalacheck"    %% "scalacheck"                 % "1.17.0"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-test-play-30"           % bootstrapVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30"          % mongoVersion,
     "uk.gov.hmrc"           %% "api-platform-test-common-domain"  % "0.10.0",
-    "org.scalatestplus"     %% "scalacheck-1-15"                  % "3.2.10.0",
+    "org.scalatestplus"     %% "scalacheck-1-15"                  % "3.2.11.0",
     "org.pegdown"            % "pegdown"                          % "1.6.0",
     "org.scalaj"            %% "scalaj-http"                      % "2.4.2",
   ).map(m => m % "test,it")
